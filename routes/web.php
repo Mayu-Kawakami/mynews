caaf9cd1aaa7a100d7a61f907875ad9260c510cb
Mayu Kawakami <koimayu.5.27@gmail.com>
2020-03-01 05:26:55 +0000

    09課題

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,5 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
-});
-
-Route::group(['prefix' => 'admin'], function() {
-    
-});
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});;
